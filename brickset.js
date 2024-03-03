edb7c69b4ce9d7dee1038826579f0bde323726f1
Gokuchan89 <albator26@gmail.com>
2024-03-03 22:13:31 +0100

    Add files via upload

---
+++ brickset.js
@@ -0,0 +1,35 @@
+function Brickset() {
+}
+
+Brickset.prototype.search = function(query) {
+  var resultArray = [];
+  var result = http().get("https://brickset.com/api/v3.asmx/getSets?apiKey=3-qx0l-RNvb-G4eYE&userHash=&params={'query':'" + encodeURIComponent(query) + "'}");
+  var rawxml = result.body;
+  if (rawxml) {
+    var xml = new XmlDocument(rawxml);
+    xml.eachChild(function(lego) {
+      var object = {};
+      object["number"] = lego.valueWithPath("number");
+      object["query"] = lego.valueWithPath("query");
+      resultArray.push(object);
+    });
+  }
+  return resultArray;
+}
+
+Brickset.prototype.getDetails = function(number) {
+  var object = {};
+  var result = http().get("https://brickset.com/api/v3.asmx/getSets?apiKey=3-qx0l-RNvb-G4eYE&userHash=&params={'number':'" + encodeURIComponent(number) + "'}");
+  var rawxml = result.body;
+  if (rawxml) {
+    var xml = new XmlDocument(rawxml);
+    var imageBase = xml.valueWithPath("imageURL");
+    var v;
+    v = lego.valueWithPath("number");
+    v = lego.valueWithPath("name");
+    v = lego.valueWithPath("year");
+    v = lego.valueWithPath("pieces");
+    v = lego.valueWithPath("minifigs");
+  }
+  return object;
+}
